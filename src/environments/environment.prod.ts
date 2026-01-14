f7668607c835e2ff35a89d9ba458147a010009fd
KhamessiTaha <taha.khamessi@gmail.com>
2026-01-14 11:59:07 +0100

    Added protected env files for API URLs

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  API_URL: 'https://your-prod-backend.com/api' // hedha baad fl prod wa9t lbackend yabda hosted!!
+};
+ // keep in mind hedha baad bch nzidouh fl .gitignore 5ater fih information sensitive mtaa api prod environment
